d8b6e47059b1044c512a0dd913595ec8c48e5efe
Rod Johnson <johnsonroda@gmail.com>
2018-02-14 04:29:37 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/atomist/nwt/AndersonApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.nwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndersonApplication.class, args);
+	}
+}
---
+++ src/test/java/com/atomist/nwt/AndersonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.atomist.nwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AndersonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
